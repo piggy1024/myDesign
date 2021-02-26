b3f9b0f6fca1483060aa3e0c46370d01d7f3d2ab
piggy1024 <1293266846@qq.com>
2021-02-27 00:47:44 +0800

    [CHG] 后台修改我的申请进度返回主数据字段;前端页面对应改变我的申请进度部分字段名

--- src/views/applicationProgress/index.vue
+++ src/views/applicationProgress/index.vue
@@ -15,27 +15,28 @@
       </el-table-column>
       <el-table-column label="申请单位" width="110">
         <template slot-scope="scope">
-          {{ scope.row.applicant }}
+          {{ scope.row.app_id.applicant }}
         </template>
       </el-table-column>
       <el-table-column label="申请联系人" width="110" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.app_name }}</span>
+          <span>{{ scope.row.app_id.app_name }}</span>
         </template>
       </el-table-column>
       <el-table-column label="审批时间" width="110" align="center">
         <template slot-scope="scope">
-          {{ scope.row.app_passTime }}
+          {{ scope.row.app_id.app_passTime }}
         </template>
       </el-table-column>
       <el-table-column label="审批原因" align="center">
         <template slot-scope="scope">
-          {{ scope.row.reason }}
+          {{ scope.row.app_id.reason }}
         </template>
       </el-table-column>
       <el-table-column label="审批状态" width="110" align="center">
         <template slot-scope="scope">
-          {{ scope.row.status }}
+          <span v-if="scope.row.app_id.status !== 0">{{ scope.row.app_id.status == 1 ? "已通过" : "被驳回" }}</span>
+          <span v-if="scope.row.app_id.status === 0">未审批</span>
         </template>
       </el-table-column>
     </el-table>
@@ -61,6 +62,7 @@ export default {
       this.listLoading = true
       getApplyList({user_id: store.getters.user_id}).then(res => {
         this.list = res.data
+        console.log(res.data);
         this.listLoading = false
       })
     }
